5099b68727e3bf23a6ed0121c597b4f746a31592
Prathamesh Sawant <prathamesh2901@gmail.com>
2020-01-18 23:18:53 -0800

    Improved Project Structure and Maintainability by organizing code in Resources and Models package

--- models/user.py
+++ models/user.py
@@ -1,7 +1,6 @@
 import sqlite3
-from flask_restful import Resource, reqparse
 
-class User:
+class UserModel:
     def __init__(self, user_id, username, password):
         self.id = user_id
         self.username = username
@@ -38,36 +37,3 @@ class User:
 
         connection.close()
         return _id
-
-class UserRegister(Resource):
-
-    parser = reqparse.RequestParser()
-    parser.add_argument('username',
-        type=str,
-        required=True,
-        help="This field cannot be blank."
-
-    )
-    parser.add_argument('password',
-        type=str,
-        required=True,
-        help="This field cannot be blank."
-
-    )
-
-    def post(self):
-        data = UserRegister.parser.parse_args()
-
-        if User.find_by_username(data['username']):
-            return {"message": "The User with that username already exists"}, 400
-
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        cursor.execute(query, (data['username'], data['password']))
-
-        connection.commit()
-        connection.close()
-
-        return {"message": "User created successfully."}, 200
